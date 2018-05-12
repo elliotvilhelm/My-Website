22568989a2e9b4d8269fc8030e89fc1c65e15ff2
Elliot <elliot@pourmand.com>
2018-05-11 23:03:36 -0700

    up and running, first push

---
+++ react-client/src/Components/FooterComponent/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          Footer component
+        </div>
+      </footer>
+    )
+  }
+}
+export default Footer;
+
