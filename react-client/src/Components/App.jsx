3866e70121900602e0b3905f5a84ab1bce0fc493
Elliot <elliot@pourmand.com>
2018-05-12 23:20:16 -0700

    material ui

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -4,17 +4,23 @@ import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
 import NavBar from './HeaderComponent/NavBar';
 import Footer from './FooterComponent/Footer';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <NavBar />
-          <Route name="home" exact path="/" component={HomePage} />
-      <Footer />
-        </div>
-      </Router>
-    )
-  }
+    render() {
+        return (
+            <Router>
+                <MuiThemeProvider>
+                <div>
+                    <NavBar />
+                    <Route name="home" exact path="/" component={HomePage} />
+                    <Footer />
+                </div>
+                </MuiThemeProvider>
+            </Router>
+        )
+    }
 }
 export default App;
--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton'
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
+
+const style = {
+    display: 'inline-block',
+    margin: '40px 32px 16px 0',
+};
+
 class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        Home page
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Paper style={style}>
+                    <Menu>
+                        <MenuItem primaryText="QLearning" />
+                        <MenuItem primaryText="About Me" />
+                    </Menu>
+                </Paper>
+            </div>
+
+        )
+    }
 }
+
 export default HomePage;
