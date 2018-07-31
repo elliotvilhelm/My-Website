a73053800c68babd5b7487f9c704e549a2f5b396
ElliotVilhelm <elliot@pourmand.com>
2018-07-30 18:25:52 -0700

    config

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    database:{
+        host: "ec2-54-83-22-244.compute-1.amazonaws.com",
+        database: "d3pgl34jjj9uso",
+        user: "jnycbkiuzgjabc",
+        port: "5432",
+        password: "bb6859633e906355a90a316c3398a784689ed387c704d3eb5b2c9b9c28b22b27",
+        ssl: true,
+    }
+}
