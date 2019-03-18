8da64101471bf793ddbe2e919c30295b444da830
ElliotVilhelm <elliot@pourmand.com>
2019-03-17 23:57:57 -0700

    haskell update

--- react-client/src/Components/Haskell.jsx
+++ react-client/src/Components/Haskell.jsx
@@ -49,10 +49,14 @@ class Haskell extends Component {
                             of the expression evaluation for a given (x, y) is translated into a pixel intensity. This is repeated 3 times
                             to map all three channels for RGB images.
                             <br/>
-                            We make a call to:
-                            <br/>
+                            <h4>Execution</h4>
+                            <pre>
+                            <code>
                             >>> emitRandomColor image_size (depth, seed)
-                            <br/>The first parameter "image_size" sets the size of the image to be generated. The next parameter "depth" sets depth of the
+                            </code>
+                            </pre>
+                            <br/>
+                            The first parameter "image_size" sets the size of the image to be generated. The next parameter "depth" sets depth of the
                             expression generation. The larger this number the more complicated the expression which is generated will be. The program also takes
                             exponentially longer time to generate images. The final parameter "seed" is used to initialize the random number generator. If you are not
                             familiar with seeding random number generators just know the value you choose to input as the seed will have a "random" effect on the image
@@ -65,21 +69,39 @@ class Haskell extends Component {
                             <div>
                                 <img src={rgb1}/>
                             </div>
+                            <br/>
                             <div>
-                                >>> emitRandomColor 150 (3, 123)<br/>
-                                R: sigmoid(tan(sin(pi * x) + ((sin(pi * x) + cos(pi * y)) / 2) + sigmoid(y)))<br/>
-                                G: sin(pi * cos(pi * cos(pi * x)*sin(pi * y))*sin(pi * cos(pi * x)*sin(pi * x)))<br/>
-                                B: sigmoid(sigmoid(sin(pi * y)))
+                                <pre>
+                                    <code>
+                                        >>> emitRandomColor 150 (3, 123)
+                                    </code>
+                                </pre>
+                                <pre>
+                                    <code>
+                                    R: sigmoid(tan(sin(pi * x) + ((sin(pi * x) + cos(pi * y)) / 2) + sigmoid(y)))<br/>
+                                    G: sin(pi * cos(pi * cos(pi * x)*sin(pi * y))*sin(pi * cos(pi * x)*sin(pi * x)))<br/>
+                                    B: sigmoid(sigmoid(sin(pi * y)))
+                                    </code>
+                                </pre>
                             </div>
                             <br/>
                             <div>
                                 <img src={rgb2}/>
                             </div>
+                            <br/>
                             <div>
-                                >>> emitRandomColor 150 (4, 4242)<br/><br/>
+                                <pre>
+                                    <code>
+                                >>> emitRandomColor 150 (4, 4242)
+                                    </code>
+                                </pre>
+                                <pre>
+                                    <code>
                                 R: cos(pi * tan((sin(pi * cos(pi * sin(pi * cos(pi * y)*sin(pi * y)))*sin(pi * tan(x + x + x))) &lt; cos(pi * cos(pi * (sin(pi * cos(pi * y)*sin(pi * y)) &lt; cos(pi * cos(pi * y)*sin(pi * x)) ? cos(pi * cos(pi * x)*sin(pi * y)) : sin(pi * cos(pi * x)*sin(pi * y))))*sin(pi * tan(y + y + x))) ? cos(pi * cos(pi * sin(pi * x))*sin(pi * tan(x + x + y))) : sin(pi * cos(pi * tan(y + x + x))*sin(pi * sigmoid(y)))) + tan(tan(y + y + y) + cos(pi * x)*sin(pi * y) + sin(pi * cos(pi * x)*sin(pi * y))) + tan(tan(y + y + y) + ((sin(pi * y) + cos(pi * y)) / 2) + sin(pi * x))))*sin(pi * sin(pi * cos(pi * sin(pi * cos(pi * y)*sin(pi * y)))*sin(pi * sin(pi * y))))<br/><br/>
                                 G: cos(pi * ((sin(pi * tan((sin(pi * cos(pi * x)*sin(pi * y)) &lt; cos(pi * cos(pi * y)*sin(pi * x)) ? cos(pi * cos(pi * x)*sin(pi * x)) : sin(pi * cos(pi * y)*sin(pi * y))) + tan(y + x + y) + sigmoid(x))) + cos(pi * sigmoid(sin(pi * cos(pi * x)*sin(pi * x))))) / 2))*sin(pi * tan(tan(((sin(pi * x) + cos(pi * x)) / 2) + tan(y + y + x) + sigmoid(x)) + sin(pi * cos(pi * sin(pi * cos(pi * y)*sin(pi * x)))*sin(pi * tan(x + x + y))) + ((sin(pi * tan(x + y + y)) + cos(pi * cos(pi * x)*sin(pi * x))) / 2)))<br/><br/>
                                 B: tan(sigmoid((sin(pi * cos(pi * (sin(pi * cos(pi * y)*sin(pi * y)) &lt; cos(pi * cos(pi * y)*sin(pi * x)) ? cos(pi * cos(pi * y)*sin(pi * x)) : sin(pi * cos(pi * x)*sin(pi * y))))*sin(pi * sin(pi * x))) &lt; cos(pi * cos(pi * sin(pi * x))*sin(pi * tan(y + y + y))) ? cos(pi * cos(pi * sin(pi * x))*sin(pi * sin(pi * x))) : sin(pi * cos(pi * ((sin(pi * y) + cos(pi * y)) / 2))*sin(pi * tan(y + x + x))))) + ((sin(pi * tan((sin(pi * cos(pi * x)*sin(pi * x)) &lt; cos(pi * cos(pi * y)*sin(pi * y)) ? cos(pi * cos(pi * y)*sin(pi * x)) : sin(pi * cos(pi * x)*sin(pi * y))) + sigmoid(x) + tan(x + x + x))) + cos(pi * (sin(pi * cos(pi * tan(y + x + y))*sin(pi * cos(pi * x)*sin(pi * y))) &lt; cos(pi * cos(pi * tan(x + y + y))*sin(pi * tan(y + x + x))) ? cos(pi * cos(pi * tan(x + x + x))*sin(pi * sin(pi * cos(pi * y)*sin(pi * y)))) : sin(pi * cos(pi * ((sin(pi * y) + cos(pi * y)) / 2))*sin(pi * sin(pi * cos(pi * x)*sin(pi * x))))))) / 2) + sin(pi * cos(pi * tan(sigmoid(x) + sin(pi * cos(pi * x)*sin(pi * x)) + sigmoid(x)))*sin(pi * sin(pi * cos(pi * x)*sin(pi * y)))))<br/>
+                                    </code>
+                                </pre>
                             </div>
                             <br/>
                             <br/>
@@ -92,7 +114,7 @@ class Haskell extends Component {
                         <div style={{height: '50px'}}/>
                         <div className="haskell2-div">
                             <div>
-                            <h3>Gallery</h3>
+                                <h3>Gallery</h3>
                             </div>
                             <div className="inline-div">
                                 <img src={rgb3}/>
@@ -101,10 +123,14 @@ class Haskell extends Component {
                                 <img src={rgb4}/>
                             </div>
                             <div className="inline-div">
+                                <pre><code>
                                 >>> emitRandomColor 150 (5, 99999)
+                                </code></pre>
                             </div>
                             <div className="inline-div">
+                                <pre><code>
                                 >>> emitRandomColor 150 (6, 12)
+                                </code></pre>
                             </div>
                             <div className={{height: '200px'}}/>
                             <div className="inline-div">
@@ -114,10 +140,14 @@ class Haskell extends Component {
                                 <img src={rgb6}/>
                             </div>
                             <div className="inline-div">
+                                <pre><code>
                                 >>> emitRandomColor 150 (4, 222)
+                                </code></pre>
                             </div>
                             <div className="inline-div">
+                                <pre><code>
                                 >>> emitRandomColor 150 (4, 666622)
+                                </code></pre>
                             </div>
                             <div className={{height: '200px'}}/>
                         </div>
